7bcc0a566d3e1db6243c360021aa5f72da578574
Ryan Miller <ryanmillergm@gmail.com>
2020-04-17 11:30:22 -0600

    Updated form styles for portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+User.create!(name: "Ryan", email: "user_1@gmail.com", password: "password", roles: "site_admin")
+User.create!(name: "Paul", email: "user_2@gmail.com", password: "password")
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic + 1}"
@@ -32,6 +35,9 @@ puts "5 skills skills created"
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/350x200"
+  )
+  Portfolio.last.technologies.create!(
+    name: "Technology #{portfolio_item}"
   )
 end
 
