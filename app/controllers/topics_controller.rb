6da5278ae5ea01c931fe1ab783a950aca99656f8
Ryan Miller <ryanmillergm@gmail.com>
2020-05-14 15:40:34 -0600

    Updates topic controller with before action

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,18 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
+  layout "blog"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :edit]}, site_admin: :all
+
   def index
-    @topics = Topic.all
+    @topics = Topic.page(params[:page]).per(5)
   end
 
   def show
+  end
+
+  private
+
+  def set_topic
     @topic = Topic.find(params[:id])
-    binding.pry
   end
 end
